b17ac880222f7d50a34b06d0e928e2c4245a9082
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-05-29 23:20:33 +0530

    DAy 1 - Set up React Done!!!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-danger">Ecommerce Website</h1>
+    </>
+  );
+};
+
+export default App;

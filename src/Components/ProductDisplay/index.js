0ebeccd8dea25ee367cdfd4ddfe5970366247058
Rohit Mathur <127666041+RohitMathur747@users.noreply.github.com>
2026-06-07 16:31:16 +0530

    day 7 - Part 3 - Two new banner lower content and side bar bannner is Done

---
+++ src/Components/ProductDisplay/index.js
@@ -0,0 +1,248 @@
+import React from "react";
+import product1 from "../../assets/images/banner2.jpg";
+import product2 from "../../assets/images/banner3.jpg";
+import product3 from "../../assets/images/banner4.jpg";
+import product4 from "../../assets/images/banner5.jpg";
+import product5 from "../../assets/images/banner6.jpg";
+import product6 from "../../assets/images/banner7.jpg";
+import Rating from "@mui/material/Rating";
+import { SlSizeFullscreen } from "react-icons/sl";
+import { IoMdHeartEmpty } from "react-icons/io";
+import Button from "@mui/material/Button";
+
+const ProductDisplayNoSlider = () => {
+  return (
+    <>
+      <div className="product-display-grid">
+        <div className="product-row w-100">
+          <div className="item productItem">
+            <div className="imgWrapper">
+              <img
+                src={product1}
+                className="productImage px-1 mt-2"
+                alt="Product 1"
+              />
+              <span className="badge badge-primary">28%</span>
+              <div className="actions">
+                <Button>
+                  <SlSizeFullscreen />
+                </Button>
+                <Button>
+                  <IoMdHeartEmpty />
+                </Button>
+              </div>
+              <div className="info">
+                <h4>Clothing For Women</h4>
+                <span className="text-success">In Stock</span>
+                <br />
+                <Rating name="size-medium" className="mt-2" defaultValue={5} />
+                <div className="d-flex">
+                  <span className="oldPrice mt-2">$20.00</span>
+                  <span className="netPrice text-danger mt-2">
+                    &nbsp; $14.00
+                  </span>
+                </div>
+              </div>
+              <div className="addToCartWrap">
+                <Button
+                  className="addToCartBtn"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item productItem">
+            <div className="imgWrapper">
+              <img
+                src={product2}
+                className="productImage px-1 mt-2"
+                alt="Product 2"
+              />
+              <span className="badge badge-primary">20%</span>
+              <div className="actions"></div>
+              <div className="info">
+                <h4>Clothing For Men</h4>
+                <span className="text-success mb-2">In Stock</span>
+                <br />
+                <Rating name="size-medium" className="mt-2" defaultValue={4} />
+                <div className="d-flex">
+                  <span className="oldPrice mt-2">$20.00</span>
+                  <span className="netPrice text-danger mt-2">
+                    &nbsp; $14.00
+                  </span>
+                </div>
+              </div>
+              <div className="addToCartWrap">
+                <Button
+                  className="addToCartBtn"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item productItem">
+            <div className="imgWrapper">
+              <img
+                src={product3}
+                className="productImage px-1 mt-2"
+                alt="Product 3"
+              />
+              <span className="badge badge-primary">15%</span>
+              <div className="actions"></div>
+              <div className="info">
+                <h4>Clothing For Kids</h4>
+                <span className="text-success mb-2">In Stock</span>
+                <br />
+                <Rating
+                  name="size-medium"
+                  className="mt-2"
+                  defaultValue={4}
+                  precision={4.5}
+                />
+                <div className="d-flex">
+                  <span className="oldPrice mt-2">$20.00</span>
+                  <span className="netPrice text-danger mt-2">
+                    &nbsp; $14.00
+                  </span>
+                </div>
+              </div>
+              <div className="addToCartWrap">
+                <Button
+                  className="addToCartBtn"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item productItem">
+            <div className="imgWrapper">
+              <img
+                src={product4}
+                className="productImage px-1 mt-2"
+                alt="Product 4"
+              />
+              <span className="badge badge-primary">15%</span>
+              <div className="actions"></div>
+              <div className="info">
+                <h4>Suits For Men</h4>
+                <span className="text-success mb-2">In Stock</span>
+                <br />
+                <Rating
+                  className="mt-2"
+                  name="size-medium"
+                  defaultValue={4}
+                  precision={4.5}
+                />
+                <div className="d-flex">
+                  <span className="oldPrice mt-2">$250.00</span>
+                  <span className="netPrice text-danger mt-2">
+                    &nbsp; $194.00
+                  </span>
+                </div>
+              </div>
+              <div className="addToCartWrap">
+                <Button
+                  className="addToCartBtn"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item productItem">
+            <div className="imgWrapper">
+              <img
+                src={product5}
+                className="productImage px-1 mt-2"
+                alt="Product 5"
+              />
+              <span className="badge badge-primary">5%</span>
+              <div className="actions"></div>
+              <div className="info">
+                <h4>Rolex</h4>
+                <span className="text-success mt-2">In Stock</span>
+                <br />
+                <Rating
+                  className="mt-2"
+                  name="size-medium"
+                  defaultValue={4}
+                  precision={4.5}
+                />
+                <div className="d-flex">
+                  <span className="oldPrice mt-2">$95000.00</span>
+                  <span className="netPrice text-danger mt-2">
+                    &nbsp; $90000.00
+                  </span>
+                </div>
+              </div>
+              <div className="addToCartWrap">
+                <Button
+                  className="addToCartBtn"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="item productItem">
+            <div className="imgWrapper">
+              <img
+                src={product6}
+                className="productImage px-1 mt-2"
+                alt="Product 6"
+              />
+              <span className="badge badge-primary">10%</span>
+              <div className="actions"></div>
+              <div className="info">
+                <h4>Iphone 17 Pro Max</h4>
+                <span className="text-success mb-2">In Stock</span>
+                <br />
+                <Rating
+                  className="mt-2"
+                  name="size-medium"
+                  defaultValue={4}
+                  precision={4.5}
+                />
+                <div className="d-flex">
+                  <span className="oldPrice mt-2">$1420.00</span>
+                  <span className="netPrice text-danger mt-2">
+                    &nbsp; $1200.00
+                  </span>
+                </div>
+              </div>
+              <div className="addToCartWrap">
+                <Button
+                  className="addToCartBtn"
+                  variant="contained"
+                  color="primary"
+                >
+                  Add to Cart
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDisplayNoSlider;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import HomeBanner from "../../Components/HomeBanner";
 import banner1 from "../../assets/images/banner1.jpg";
 import banner2 from "../../assets/images/verticalbanner2.jpg";
+import banner3 from "../../assets/images/verticalbanner3.jpg";
 import Button from "@mui/material/Button";
 import { FaArrowRight } from "react-icons/fa";
 import ProductItem from "../../Components/ProductItem";
 import HomeCat from "../../Components/HomeCat";
+import ProductDisplayNoSlider from "../../Components/ProductDisplay";
+import lowban1 from "../../assets/images/lowerbanner1.jpg";
+import lowban2 from "../../assets/images/lowerbanner2.jpg";
 
 const Home = () => {
   return (
@@ -23,6 +27,10 @@ const Home = () => {
               </div>
               <div className="banner">
                 <img src={banner2} className="cursor w-100 mt-5" />
+              </div>
+
+              <div className="banner">
+                <img src={banner3} className="cursor w-100 mt-5" />
               </div>
             </div>
             <div className="col-md-9 productRow">
@@ -54,7 +62,16 @@ const Home = () => {
                 </Button>
               </div>
 
-              <ProductItem />
+              <ProductDisplayNoSlider />
+            </div>
+
+            <div className="lowerslider row">
+              <div className="col-sm-6">
+                <img src={lowban1} className="w-100" alt="lower banner 1" />
+              </div>
+              <div className="col-sm-6">
+                <img src={lowban2} className="w-100" alt="lower banner 2" />
+              </div>
             </div>
           </div>
         </div>
